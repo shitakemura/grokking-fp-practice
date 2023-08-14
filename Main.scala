89c87090d92a835a3a086794e674459d7ad76e97
shitakemura <shitakemura@gmail.com>
2023-08-14 22:27:47 +0900

    ~ p141

--- Main.scala
+++ Main.scala
@@ -67,3 +67,26 @@
 
 // languages.map(_.name)
 // languages.filter(_.year > 2000)
+
+// 5
+
+// case class Book(title: String, authors: List[String])
+
+// val books = List(
+//     Book("FP in Scala", List("Chiusano", "Bjarnason")),
+//     Book("The Hobbit", List("Tolkien")),
+//     Book("Modern Java in Action", List("Urma", "Fusco", "Mycroft"))
+// )
+
+// books.map(_.title).filter(_.contains("Scala")).size
+
+// case class Movie(title: String)
+
+// def bookAdaptions(author: String): List[Movie] = {
+//     if (author == "Tolkien") List(Movie("An Unexpected Journey"), Movie("The Desolation of Smaug"))
+//     else List.empty
+// }
+
+// def recommendationFeed(books: List[Book]): List[String] = {
+//     books.flatMap(_.authors).map(bookAdaptions)
+// }
