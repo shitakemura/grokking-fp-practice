f666aa1338eb573195720640b9ebf4a7e351ee95
shitakemura <shitakemura@gmail.com>
2023-08-17 08:28:47 +0900

    ~ p167

--- Main.scala
+++ Main.scala
@@ -300,3 +300,44 @@ for {
     a <- Set(1, 2)
     b <- List(2, 1)
 } yield a * b
+
+// 5.31
+
+case class Event(name: String, start: Int, end: Int)
+
+def parse(name: String, start: Int, end: Int): Event = {
+    if (name.size > 0 && end < 3000 && start <= end)
+        Event(name, start, end)
+    else
+        null
+}
+
+def parse(name: String, start: Int, end: Int): Option[Event] = {
+    if (name.size > 0 && end < 3000 && start <= end)
+        Some(Event(name, start, end))
+    else
+        None
+}
+
+def validateName(name: String): Option[String] = {
+    if (name.size > 0) Some(name) else None
+}
+
+def validateEnd(end: Int): Option[Int] = {
+    if (end < 3000) Some(end) else None
+}
+
+def validateStart(start: Int, end: Int): Option[Int] = {
+    if (start <= end) Some(start) else None
+}
+
+def parse(name: String, start: Int, end: Int): Option[Event] = {
+    for {
+        validName <- validateName(name)
+        validEnd <- validateEnd(end)
+        validStart <- validateStart(start, end)
+    } yield Event(validName, validStart, validEnd)
+}
+
+parse("Apollo Program", 1961, 1972)
+parse("", 1939, 1945)
