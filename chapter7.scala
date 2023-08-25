014fd8e1085da6153617e9278a73d28a8134f1de
shitakemura <shitakemura@gmail.com>
2023-08-25 09:25:17 +0900

    ~ p248

--- chapter7.scala
+++ chapter7.scala
@@ -161,3 +161,40 @@ def searchArtists(artists: List[Artist], genres: List[String], locations: List[S
         ))    
         )
     }
+
+// 7.18
+case class User(name: String, city: Option[String], favoriteArtists: List[String])
+
+val users = List(
+    User("Alice", Some("Melbourne"), List("Bee Gees")),
+    User("Bob", Some("Lagos"), List("Bee Gees")),
+    User("Eve", Some("Tokyo"), List.empty),
+    User("Mallory", None, List("Metallica", "Bee Gees")),
+    User("Trent", Some("Buenos Aires"), List("Led Zeppelin"))
+)
+
+// def f1() = users.filter(user => user.city.forall(_.contains("Melbourne")))
+// def f2() = users.filter(user => user.city.exists(_.contains("Lagos")))
+// def f3() = users.filter(user => user.favoriteArtists.exists(_.contains( "Bee Gees")))
+// def f4() = users.filter(user => user.city.exists(_.startsWith("T")))
+// def f5() = users.filter(user => user.favoriteArtists.forall(_.length > 8))
+// def f6() = users.filter(user => user.favoriteArtists.exists(_.startsWith("M")))
+
+def f1(users: List[User]): List[User] = {
+    users.filter(_.city.forall(_ == "Melbourne"))
+}
+def f2(users: List[User]): List[User] = {
+    users.filter(_.city.contains("Lagos"))
+}
+def f3(users: List[User]): List[User] = {
+    users.filter(_.favoriteArtists.contains("Bee Gees"))
+}
+def f4(users: List[User]): List[User] = {
+    users.filter(_.city.exists(_.startsWith("T")))
+}
+def f5(users: List[User]): List[User] = {
+    users.filter(_.favoriteArtists.forall(_.length > 8))
+}
+def f6(users: List[User]): List[User] = {
+    users.filter(_.favoriteArtists.exists(_.startsWith("M")))
+}
