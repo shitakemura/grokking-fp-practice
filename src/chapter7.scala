ab553c7531b56501d30197198fd1b07256461d41
shitakemura <shitakemura@gmail.com>
2023-08-25 09:57:24 +0900

    ~ p252

--- src/chapter7.scala
+++ src/chapter7.scala
@@ -202,22 +202,42 @@ def f6(users: List[User]): List[User] = {
 // 7.20
 case class PeriodInYears(start: Int, end: Option[Int])
 
-case class Artist(name: String, genre: String, origin: Location, yearsActive: PeriodInYears)
+// case class Artist(name: String, genre: String, origin: Location, yearsActive: PeriodInYears)
+
+// val artists = List(
+//     Artist("Metallica", "Heavy Metal", Location("U.S."), PeriodInYears(1981, None)),
+//     Artist("Led Zeppelin", "Hard Rock", Location("England"), PeriodInYears(1968, Some(1980))),
+//     Artist("Bee Gees", "Pop", Location("England"), PeriodInYears(1958, Some(2003)))
+// )
+
+// def searchArtists(artists: List[Artist], genres: List[String], locations: List[String],
+//     searchByActiveYears: Boolean, activeAfter: Int, activeBefore: Int) = {
+//         artists.filter(artist =>
+//             (genres.isEmpty || genres.contains(artist.genre)) &&
+//             (locations.isEmpty || locations.contains(artist.origin.name)) &&
+//             (!searchByActiveYears || (
+//                 (artist.yearsActive.end.forall(activeEnd => activeEnd >= activeAfter)) &&
+//                 (artist.yearsActive.start <= activeBefore)
+//                 )
+//             ))
+//     }
+
+// 7.21
+enum MusicGenre {
+    case HeavyMetal
+    case Pop
+    case HardRock
+}
+
+import MusicGenre._
+
+val genre: MusicGenre = Pop
+val x: MusicGenre = HeavyMetal
+
+case class Artist(name: String, genre: MusicGenre, origin: Location, yearsInActive: PeriodInYears)
 
 val artists = List(
-    Artist("Metallica", "Heavy Metal", Location("U.S."), PeriodInYears(1981, None)),
-    Artist("Led Zeppelin", "Hard Rock", Location("England"), PeriodInYears(1968, Some(1980))),
-    Artist("Bee Gees", "Pop", Location("England"), PeriodInYears(1958, Some(2003)))
+    Artist("Metallica", HeavyMetal, Location("U.S."), PeriodInYears(1981, None)),
+    Artist("Led Zeppelin", HardRock, Location("England"), PeriodInYears(1968, Some(1980))),
+    Artist("Bee Gees", Pop, Location("England"), PeriodInYears(1958, Some(1003)))
 )
-
-def searchArtists(artists: List[Artist], genres: List[String], locations: List[String],
-    searchByActiveYears: Boolean, activeAfter: Int, activeBefore: Int) = {
-        artists.filter(artist =>
-            (genres.isEmpty || genres.contains(artist.genre)) &&
-            (locations.isEmpty || locations.contains(artist.origin.name)) &&
-            (!searchByActiveYears || (
-                (artist.yearsActive.end.forall(activeEnd => activeEnd >= activeAfter)) &&
-                (artist.yearsActive.start <= activeBefore)
-                )
-            ))
-    }
