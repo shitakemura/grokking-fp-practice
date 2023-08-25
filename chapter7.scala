40d4cb10223c64c68350626870ba07e73d0cb57b
shitakemura <shitakemura@gmail.com>
2023-08-25 09:40:33 +0900

    ~ p249

--- chapter7.scala
+++ chapter7.scala
@@ -143,24 +143,24 @@ object model {
 
 import model._
 
-case class Artist(name: String, genre: String, origin: Location,
-    yearsActiveStart: Int, yearsActiveEnd: Option[Int])
+// case class Artist(name: String, genre: String, origin: Location,
+//     yearsActiveStart: Int, yearsActiveEnd: Option[Int])
 
 val a = Artist("Metallica", "Heavy Metal", Location("U.S."), 1981, None)
 val b = Artist("Led Zeppelin", "Hard Rock", Location("England"), 1968, Some(1980))
 
 // 7.14
-def searchArtists(artists: List[Artist], genres: List[String], locations: List[String],
-    searchByActiveYears: Boolean, activeAfter: Int, activeBefore: Int) = {
-        artists.filter(artist =>
-        (genres.isEmpty || genres.contains(artist.genre)) &&
-        (locations.isEmpty || locations.contains(artist.origin.name)) &&
-        (!searchByActiveYears || (
-            (artist.yearsActiveEnd.forall(activeEnd => activeEnd >= activeAfter)) &&
-            (artist.yearsActiveStart <= activeBefore)
-        ))    
-        )
-    }
+// def searchArtists(artists: List[Artist], genres: List[String], locations: List[String],
+//     searchByActiveYears: Boolean, activeAfter: Int, activeBefore: Int) = {
+//         artists.filter(artist =>
+//         (genres.isEmpty || genres.contains(artist.genre)) &&
+//         (locations.isEmpty || locations.contains(artist.origin.name)) &&
+//         (!searchByActiveYears || (
+//             (artist.yearsActiveEnd.forall(activeEnd => activeEnd >= activeAfter)) &&
+//             (artist.yearsActiveStart <= activeBefore)
+//         ))    
+//         )
+//     }
 
 // 7.18
 case class User(name: String, city: Option[String], favoriteArtists: List[String])
@@ -198,3 +198,26 @@ def f5(users: List[User]): List[User] = {
 def f6(users: List[User]): List[User] = {
     users.filter(_.favoriteArtists.exists(_.startsWith("M")))
 }
+
+// 7.20
+case class PeriodInYears(start: Int, end: Option[Int])
+
+case class Artist(name: String, genre: String, origin: Location, yearsActive: PeriodInYears)
+
+val artists = List(
+    Artist("Metallica", "Heavy Metal", Location("U.S."), PeriodInYears(1981, None)),
+    Artist("Led Zeppelin", "Hard Rock", Location("England"), PeriodInYears(1968, Some(1980))),
+    Artist("Bee Gees", "Pop", Location("England"), PeriodInYears(1958, Some(2003)))
+)
+
+def searchArtists(artists: List[Artist], genres: List[String], locations: List[String],
+    searchByActiveYears: Boolean, activeAfter: Int, activeBefore: Int) = {
+        artists.filter(artist =>
+            (genres.isEmpty || genres.contains(artist.genre)) &&
+            (locations.isEmpty || locations.contains(artist.origin.name)) &&
+            (!searchByActiveYears || (
+                (artist.yearsActive.end.forall(activeEnd => activeEnd >= activeAfter)) &&
+                (artist.yearsActive.start <= activeBefore)
+                )
+            ))
+    }
