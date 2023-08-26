da05838cbc0f8904c0d34c03f1234390a41de511
shitakemura <shitakemura@gmail.com>
2023-08-27 08:12:22 +0900

    ~ p272

---
+++ src/8-2.scala
@@ -0,0 +1,7 @@
+// 8-2
+
+case class MeetingTime(startHour: Int, endHour: Int)
+
+def calendarEntriesApiCall(name: String): List[MeetingTime] = ???
+def createMeetingApiCall(name: List[String], meetingTime: MeetingTime): Unit = ???
+
