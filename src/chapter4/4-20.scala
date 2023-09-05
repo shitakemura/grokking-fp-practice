0da41809892853775c73e9cf8d7c37cad3f0f497
shitakemura <shitakemura@gmail.com>
2023-09-05 21:09:36 +0900

    ~ p92

---
+++ src/chapter4/4-20.scala
@@ -0,0 +1,15 @@
+// 4.20
+
+// 関数毎に１つのビジネス要件を実装することに焦点を合わせる（複数のことを行わない）
+
+def score(word: String): Int = word.replaceAll("a", "").length
+
+def bonus(word: String): Int = if (word.contains("c")) 5 else 0
+
+val words = List("ada", "scala", "java", "rust", "haskell")
+
+def rankedWords(wordScore: String => Int, words: List[String]): List[String] = {
+    words.sortBy(wordScore).reverse
+}
+
+val res = rankedWords(w => score(w) + bonus(w), words)
