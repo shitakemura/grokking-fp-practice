020e42c2907bc49bf1d5d28b888276fb4a6d7e36
shitakemura <shitakemura@gmail.com>
2023-09-07 08:33:21 +0900

    ~ p105

--- src/chapter4/4-30.scala
+++ src/chapter4/4-30.scala
@@ -18,3 +18,17 @@ def highScoringWords(wordScore: String => Int, words: List[String]): List[String
 
 val words = List("ada", "haskell", "scala", "java", "rust")
 val res = highScoringWords(w => score(w) + bonus(w) - penalty(w), words)
+
+// 4.33
+
+def len(word: String): Int = word.length
+val res1 = List("scala", "rust", "ada").filter(word => len(word) < 5)
+
+def numOfS(word: String): Int = word.length - word.replaceAll("s", "").length
+val res2 = List("rust", "ada").filter(w => numOfS(w) >= 3)
+
+def odd(num: Int): Boolean = num % 2 == 1
+val res3 = List(5, 1, 2, 4, 0).filter(odd)
+
+def largerThanFour(num: Int): Boolean = num > 4
+val res4 = List(5, 1, 4, 2, 0).filter(largerThanFour)
