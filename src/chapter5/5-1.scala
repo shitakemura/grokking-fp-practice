c25820ec7e8e5f5ae8cb3d418a33f6b82b7bc137
shitakemura <shitakemura@gmail.com>
2023-09-12 22:17:35 +0900

    ~ p138

--- src/chapter5/5-1.scala
+++ src/chapter5/5-1.scala
@@ -5,7 +5,18 @@ case class Book(title: String, authors: List[String])
 val books = List(
     Book("FP in Scala", List("Chiusano", "Bjarnason")),
     Book("The Hobbit", List("Tolkien")),
-    Book("Modern Java in Action", List("Urma", "Fusco", "Mycroft"))
 )
 
 val res = books.map(_.title).filter(_.contains("Scala")).size
+
+// 5.2
+
+case class Movie(title: String)
+
+def bookAdaptations(author: String): List[Movie] = {
+    if (author == "Tolkien")
+        List(Movie("An Unexpected Journey"), Movie("The Desolation of Smaug"))
+    else List.empty
+}
+
+def recommendationFeed(books: List[Book]) = ???
