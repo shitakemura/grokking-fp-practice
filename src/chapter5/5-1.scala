6aa090e87d708508bbb79ca28caa3449c52caad1
shitakemura <shitakemura@gmail.com>
2023-09-13 08:41:14 +0900

    ~ p141

--- src/chapter5/5-1.scala
+++ src/chapter5/5-1.scala
@@ -20,3 +20,17 @@ def bookAdaptations(author: String): List[Movie] = {
 }
 
 def recommendationFeed(books: List[Book]) = ???
+
+// 5.4
+
+val res1 = books.map(_.authors)
+val res2 = books.map(_.authors).flatten
+val res3 = books.flatMap(_.authors)
+
+val authors = List("Chiusano", "Bjarnason", "Tolkien")
+val res4 = authors.map(bookAdaptations)
+val res5 = authors.map(bookAdaptations).flatten
+
+val res6 = books
+    .flatMap(_.authors)
+    .flatMap(bookAdaptations)
