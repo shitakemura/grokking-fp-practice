05d404185d0925f9c1f0ba090adb27e9953b84ed
shitakemura <shitakemura@gmail.com>
2023-09-15 09:27:21 +0900

    ~ p151

--- src/chapter5/5-12.scala
+++ src/chapter5/5-12.scala
@@ -17,3 +17,32 @@ val res13 = for {
     author <- book.authors
     movie <- bookAdaptations(author)
 } yield s"You may like ${movie.title}, " + s"because you liked $author's ${book.title}"
+
+// 5.15
+
+val xs = List(1)
+val ys = List(-2, 7)
+
+val res15a = xs.flatMap(x =>
+    ys.map(y =>
+        Point(x, y)    
+    )    
+)
+
+case class Point3d(x: Int, y: Int, z: Int)
+
+val zs = List(3, 4)
+
+val res15b = for {
+    x <- xs
+    y <- ys
+    z <- zs
+} yield Point3d(x, y, z)
+
+val res15c = xs.flatMap(x =>
+    ys.flatMap(y =>
+        zs.map(z =>
+            Point3d(x, y, z)
+        )    
+    )    
+)
