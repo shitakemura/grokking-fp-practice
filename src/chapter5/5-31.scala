0ee6b7ca9fee0f8ec4afb881ccbff1df330dfb92
shitakemura <shitakemura@gmail.com>
2023-09-17 14:13:57 +0900

    ~ p171

--- src/chapter5/5-31.scala
+++ src/chapter5/5-31.scala
@@ -20,3 +20,54 @@ def parse(name: String, start: Int, end: Int): Option[Event] = {
     else
         None
 }
+
+// 5.33
+
+case class Event(name: String, start: Int, end: Int)
+
+def validateName(name: String): Option[String] = {
+    if (name.size > 0) Some(name) else None
+}
+
+def validateEnd(end: Int): Option[Int] = {
+    if (end < 3000) Some(end) else None
+}
+
+def validateStart(start: Int, end: Int): Option[Int] = {
+    if (start <= end) Some(start) else None
+}
+
+def parse(name: String, start: Int, end: Int): Option[Event] = {
+    for {
+        validName <- validateName(name)
+        validEnd <- validateEnd(end)
+        validStart <- validateStart(start, end)
+    } yield Event(validName, validStart, validEnd)
+}
+
+val res33_1 = parse("Apollo Program", 1961, 1972)
+val res33_2 = parse("", 1939, 1945)
+
+// 5.34
+
+def validateLength(start: Int, end: Int, minLength: Int): Option[Int] = {
+    val length = end - start
+    if (length >= minLength) Some(length) else None
+}
+
+def parseLongEvent(name: String, start: Int, end: Int, minLength: Int): Option[Event] = {
+    for {
+        validName <- validateName(name)
+        validEnd <- validateEnd(end)
+        validStart <- validateStart(start, end)
+        validLength <- validateLength(start, end, minLength)
+    } yield Event(validName, validStart, validEnd)
+}
+
+val res34_1 = parseLongEvent("Apollo Program", 1961, 1972, 10)
+
+val res34_2 = parseLongEvent("World War 2", 1939, 1945, 10)
+
+val res34_3 = parseLongEvent("", 1939, 1945, 10)
+
+val res34_4 = parseLongEvent("Apollo Program", 1972, 1961, 10)
