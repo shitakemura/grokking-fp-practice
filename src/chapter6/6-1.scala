781d1ab2ef3ce9760964e9aa9fa5c28367f85531
shitakemura <shitakemura@gmail.com>
2023-09-17 22:23:49 +0900

    ~ p176

---
+++ src/chapter6/6-1.scala
@@ -0,0 +1,15 @@
+// 6.1
+
+// 6.2
+
+// ステップ1: 高階関数のおさらい
+// ステップ2: Optionを使ってエラーを通知する
+// ステップ3: 関数型エラー処理と検査例外
+// ステップ4: 多くのエラーを一度に処理する
+// ステップ5: Eitherを使って記述的エラーを通知する
+
+// 6.3
+
+case class TvShow(name: String, start: Int, end: Int)
+
+def sortShows(shows: List[TvShow]): List[TvShow] = ???
