2f514a73c868e36db9bd87615ee3439486f5d29e
shitakemura <shitakemura@gmail.com>
2023-09-20 08:04:02 +0900

    ~ p192

--- src/chapter6/6-12.scala
+++ src/chapter6/6-12.scala
@@ -11,3 +11,38 @@ def parseShow(rawShow: String): Option[TvShow] = {
         yearEnd <- extractYearEnd(rawShow)
     } yield TvShow(name, yearStart, yearEnd)
 }
+
+// 6.15
+// Optionを使うときには、エラーを表す値としてNoneを使う
+// 関数がOptionを返すとクライアントがエラーの可能性に対処せざるを得なくなる
+
+// 6.16
+// 合成可能性: 独立した別々の値を組み合わせて別の値を計算できる仕組み
+
+// 6.17
+
+// def extraYearStart(rawShow: String): Option[Int] = {
+//     val bracketOpen = rawShow.indexOf('(')
+//     val dash = rawShow.indexOf('-')
+//     val yearStartOpt = if (bracketOpen != -1 && dash > bracketOpen + 1)
+//         Some(rawShow.substring(bracketOpen + 1, dash))
+//     else None
+    
+//     yearStartOpt.map(yearStr => yearStr.toIntOption).flatten
+// }
+
+def extractYearStart(rawShow: String): Option[Int] = {
+    val bracketOpen = rawShow.indexOf('(')
+    val dash = rawShow.indexOf('-')
+
+    for {
+        yearStr <- if (bracketOpen != -1 && dash > bracketOpen + 1)
+                        Some(rawShow.substring(bracketOpen + 1, dash))
+                   else None
+        year <- yearStr.toIntOption
+    } yield year
+}
+
+val res6_18_1 = extractYearStart("Breaking Bad (2008-2013)")
+val res6_18_2 = extractYearStart("Mad Men (-1015)")
+val res6_18_3 = extractYearStart("(2002 - N/A) The Wire")
