e92457da3ff04860b29f86b25093b69062de49c5
shitakemura <shitakemura@gmail.com>
2023-09-22 10:25:57 +0900

    ~ p215

--- src/chapter6/6-30.scala
+++ src/chapter6/6-30.scala
@@ -7,3 +7,40 @@ def parseShows(rawShows: List[String]): List[TvShow] = {
 }
 
 val res6_30 = parseShows(rawShows)
+
+// 6.36
+
+// オールオアナッシング型のエラー処理ロジック
+
+def addOrResign(parsedShows: Option[List[TvShow]], newParsedShow: Option[TvShow]): Option[List[TvShow]] = {
+    for {
+        shows <- parsedShows
+        parsedShow <- newParsedShow
+    } yield shows.appended(parsedShow)
+}
+
+// 6.38
+
+// 2種類のエラー処理戦略
+
+// ベストエフォート型のエラー処理
+
+def parseShows(rawShows: List[String]): List[TvShow] = {
+    rawShows
+        .map(parseShow)
+        .map(_.toList)
+        .flatten
+}
+
+// オールオアナッシング型のエラー処理
+
+def parseShows(rawShows: List[String]): Option[List[TvShow]] = {
+    val initialResult = Some(List.empty)
+
+    rawShows
+        .map(parseShow)
+        .foldLeft(initialResult)(addOrResign)
+}
+
+
+val res6_40 = parseShows(rawShows)
