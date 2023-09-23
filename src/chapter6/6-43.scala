8fd3258da2ab21cd808e6b45647638b81d6eebf8
shitakemura <shitakemura@gmail.com>
2023-09-23 22:24:03 +0900

    ~ p222

--- src/chapter6/6-43.scala
+++ src/chapter6/6-43.scala
@@ -30,3 +30,36 @@ def parseShowsWithOption(rawShows: List[String]): Option[List[TvShow]] = ???
 
 // Right[List[TvShow]] or Left[String]を返す
 def parseShowsWithEither(rasShows: List[String]): Either[String, List[TvShow]] = ???
+
+// 6.46
+
+def extractYearStart(rawShow: String): Either[String, Int] = {
+    val bracketOpen = rawShow.indexOf('(')
+    val dash = rawShow.indexOf('-')
+    val yearStrEither = if (bracketOpen != -1 && dash > bracketOpen + 1)
+        Right(rawShow.substring(bracketOpen + 1, dash))
+    else
+        Left(s"Can't extract start year from $rawShow")
+    yearStrEither
+        .map(yearStr => 
+                yearStr
+                    .toIntOption
+                    .toRight(s"Can't parse $yearStr")
+            ).flatten
+}
+
+def extractYearStartWithFor(rawShow: String): Either[String, Int] = {
+    val bracketOpen = rawShow.indexOf('(')
+    val dash = rawShow.indexOf('-')
+    for {
+        yearStr <- if (bracketOpen != -1 && dash > bracketOpen + 1)
+                      Right(rawShow.substring(bracketOpen + 1, dash))
+                   else Left(s"Can't extract start year from $rawShow")
+        year <- yearStr.toIntOption.toRight(s"Can't parse $yearStr")
+    } yield year
+}
+
+val res6_46_1 = extractYearStartWithFor("The Wire (2002-2008)")
+val res6_46_2 = extractYearStartWithFor("The Wire (-2008)")
+val res6_46_3 = extractYearStartWithFor("The Wire (oops-2008)")
+val res6_46_4 = extractYearStartWithFor("The Wire (2002-)")
