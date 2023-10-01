a87bf144eb95e331354c05224e9e4f779ae04396
shitakemura <shitakemura@gmail.com>
2023-10-01 10:02:56 +0900

    ~ p262

--- src/chapter7/7-31.scala
+++ src/chapter7/7-31.scala
@@ -32,3 +32,44 @@ object model {
 
     case class Playlist(name: String, kind: PlaylistKind, songs: List[Song])
 }
+
+import model._, model.MusicGenre._, model.PlaylistKind._
+
+val fooFighters = Artist("Foo Fighters")
+val playlist1 = Playlist(
+    "This is Foo Fighters", 
+    BasedOnArtist(fooFighters),
+    List(
+        Song(fooFighters, "Break Out"), 
+        Song(fooFighters, "Learn To Fly")
+    )   
+)
+
+val playlist2 = Playlist(
+    "Deep Focus",
+    BasedOnGenres(Set(House, Funk)),
+    List(
+        Song(Artist("Daft Punk"), "One More Time"),
+        Song(Artist("The Chemical Brothers"), "Hey Boy Hey Girl")
+    )
+)
+
+val playlist3 = Playlist(
+    "My Playlist",
+    CuratedByUser(User("Michal Plachta")),
+    List(
+        Song(fooFighters, "My Hero"),
+        Song(Artist("Iron Maiden"), "The Tropper")
+    )
+)
+
+def gatherSongs(playlists: List[Playlist], artist: Artist, genre: MusicGenre): List[Song] = {
+    playlists.foldLeft(List.empty[Song])((songs, playlist) =>
+        val matchingSongs = playlist.kind.match {
+            case CuratedByUser(user) => playlist.songs.filter(_.artist == artist)
+            case BasedOnArtist(playArtist) => if (playArtist == artist) playlist.songs else List.empty
+            case BasedOnGenres(genres) => if (genres.contains(genre)) playlist.songs else List.empty 
+        }
+        songs.appendedAll(matchingSongs)
+    )
+}
