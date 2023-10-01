007029deb23edfd511b509dac32a12c51f267933
shitakemura <shitakemura@gmail.com>
2023-10-02 08:11:34 +0900

    ~ p268

--- src/chapter7/7-34.scala
+++ src/chapter7/7-34.scala
@@ -28,3 +28,55 @@ def searchArtists(artists: List[Artist], requiredConditions: List[SearchConditio
         )    
     )
 }
+
+// 7.38
+
+case class PeriodInYears(start: Int, end: Int)
+
+enum YearsActive {
+    case StillActive(since: Int, previousPeriods: List[PeriodInYears])
+    case ActiveInPast(periods: List[PeriodInYears])
+}
+
+case class Artist(name: String, genre: MusicGenre, origin: Location, yearsActive: YearsActive)
+
+enum SearchCondition {
+    case SearchByGenre(genres: List[MusicGenre])
+    case SearchByOrigin(locations: List[Location])
+    case SearchByActiveYears(period: PeriodInYears)
+    case SearchByActiveLength(howLong: Int, until: Int)
+}
+
+import SearchCondition._, YearsActive._
+
+def periodOverlapsWithPeriods(checkedPeriod: PeriodInYears, periods: List[PeriodInYears]): Boolean = {
+    periods.exists(p => p.start <= checkedPeriod.end && p.end >= checkedPeriod.start)
+}
+
+def wasArtistActive(artist: Artist, searchedPeriod: PeriodInYears): Boolean = {
+    artist.yearsActive match {
+        case StillActive(since, previousPeriods) => since <= searchedPeriod.end || periodOverlapsWithPeriods(searchedPeriod, previousPeriods)
+        case ActiveInPast(periods) => periodOverlapsWithPeriods(searchedPeriod, periods)
+    }
+}
+
+def activeLength(artist: Artist, currentYear: Int): Int = {
+    val periods = artist.yearsActive match {
+        case StillActive(since, previousPeriods) => previousPeriods.appended(PeriodInYears(since, currentYear))
+        case ActiveInPast(periods) => periods
+    }
+    periods.map(p => p.end - p.start).foldLeft(0)((x, y) => x + y)
+}
+
+def searchArtists(artists: List[Artist], requiredConditions: List[SearchCondition]): List[Artist] = {
+    artists.filter(artist =>
+        requiredConditions.forall(condition =>
+            condition match {
+                case SearchByGenre(genres) => genres.contains(artist.genre)
+                case SearchByOrigin(locations) => locations.contains(artist.origin)
+                case SearchByActiveYears(period) => wasArtistActive(artist, period)
+                case SearchByActiveLength(howLong, until) => activeLength(artist, until) >= howLong
+            }    
+        )    
+    )
+}
